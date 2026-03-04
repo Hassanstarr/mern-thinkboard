67812c8061ba57030047dca5a1e02749b5bc6947
HassanStarr <hassanasif419@gmail.com>
2026-03-04 05:52:47 +0500

    update code of deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from "cors"
+import path from "path";
 
 import { connectDB } from './config/db.js';
 import notesRouter from "./routes/notesRoutes.js";
@@ -18,11 +19,14 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors({
-  origin:"http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin:"http://localhost:5173",
+  }));
+}
 app.use(express.json()); //this middleware will parse JSON bodies: req.body
 app.use(rateLimiter)
 
@@ -35,6 +39,13 @@ app.use(rateLimiter)
 // );
 
 app.use("/api/notes", notesRouter);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 // Only when DB connect do we want our app to run, if it failed then app won't run this way
 // We want app to run after the database is connected (it takes some time to connect to database then run the server)
