fbccf973944b55cbcaf5adbda04b433d2839af86
HassanStarr <hassanasif419@gmail.com>
2026-03-13 12:24:46 +0500

    hosting6

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // Remove dotenv — it doesn't work in the frontend
 
 // Use REACT_APP_BACKEND_URL for production (Vercel) and fallback to localhost in dev
-const BASE_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5001/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001/api";
 const api = axios.create({
   baseURL: BASE_URL,
 });
